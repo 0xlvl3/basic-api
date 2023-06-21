7cf3ac20138d4bf3ef1540265e802a66fd2e433e
0xlvl3 <cryptochoccy@gmail.com>
2023-06-21 12:49:42 +1000

    start of api

--- api/main.go
+++ api/main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 // first make a basic api, that has a user, which we can then pull with endpoints.
 
-import "fmt"
-
 func main() {
-	fmt.Println("vim-go")
+
+	lp := flag.String("lp", ":8080", "Listening port")
+	flag.Parse()
+
+	client, err := mongo.Connect(context.TODO, options.Client().ApplyURI(db.URI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
